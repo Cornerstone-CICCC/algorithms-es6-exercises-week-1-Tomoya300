a1ad45d62221a738fab218cc2e6ee1dc22373846
Tomoya <flyingfox193807@gmail.com>
2024-09-20 12:15:29 -0700

    finished ex10.js

--- exercises/ex10.js
+++ exercises/ex10.js
@@ -1,10 +1,14 @@
 /*
 Rewrite the following while loop as a for loop:
-
-let x = 9;
-while (x >= 1) {
-  console.log("hello " + x);
-  x = x - 1;
-}
-
 */
+
+// let x = 9;
+// while (x >= 1) {
+//   console.log("hello " + x);
+//   x = x - 1;
+// }
+
+
+for (let i = 9; i >= 1; i--) {
+  console.log("hello " + i)
+}
