91eff064062222ff844020df9a5e178fb6c334b6
Tomoya <flyingfox193807@gmail.com>
2024-09-22 20:53:17 -0700

    finished ex21.js

--- exercises/ex21.js
+++ exercises/ex21.js
@@ -6,3 +6,9 @@ TIP: The 1st element of any array has an index of 0.
 
 Afterwards, print out the prices array to the console.
 */
+
+let prices = [1.23, 48.11, 90.11, 8.50, 9.99, 1.00, 1.10, 67.00]
+prices.splice(0, 1, 1.50)
+prices.splice(2, 1, 95.11)
+prices.splice(6, 1, 1.05)
+console.log(prices)
