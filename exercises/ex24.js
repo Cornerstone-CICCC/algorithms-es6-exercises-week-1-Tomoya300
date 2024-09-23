74be85dc461cb15411250ad377de9aff386dfe6c
Tomoya <flyingfox193807@gmail.com>
2024-09-22 23:25:06 -0700

    finished ex24.js

--- exercises/ex24.js
+++ exercises/ex24.js
@@ -20,3 +20,14 @@ console.log(lastIndexOf([ 5, 5, 5 ], 5), "=?", 2);
 console.log(lastIndexOf([], 3), "=?", -1);
 
 */
+
+let numbers = [0, 1, 4, 1, 2]
+function lastIndexOf(array, number) {
+    for (let i = array.length - 1; i >= 0; i--) {
+        if (array[i] === number) {
+            return i
+        } 
+    }
+    return -1
+}
+console.log(lastIndexOf(numbers, 1))
